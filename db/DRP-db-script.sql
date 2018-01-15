3287cabf6d2608214d906465285df1a3a2566623
MrQuJL <909301740@qq.com>
2018-01-15 12:10:34 +0800

    添加用户表

--- db/DRP-db-script.sql
+++ db/DRP-db-script.sql
@@ -1,6 +1,8 @@
 create database drp;
 use drp;
 
+# 业务功能部分的表
+# 客户信息表
 create table customer(
 	customer_id varchar(8) primary key,
 	customer_simple_name varchar(20) not null,
@@ -14,14 +16,14 @@ create table customer(
 	delivery_address varchar(100),
 	invoice_address varchar(100)	
 )
-
+# 商品表
 create table product(
 	product_id varchar(8) primary key,
 	product_name varchar(30) not null,
 	quantity decimal(18,2) not null,
 	avg_buy_price decimal(18,2) not null
 )
-
+# 供应商信息表
 create table supplier(
 	supplier_id varchar(8) primary key,
 	supplier_simple_name varchar(20) not null,
@@ -34,14 +36,14 @@ create table supplier(
 	company_address varchar(100),
 	factory_address varchar(100)
 )
-
+# 采购单主表
 create table purchase_master(
 	purchase_id varchar(8) primary key,
 	purchase_date datetime,
 	supplier_id varchar(8),
 	subtotal decimal(18,2)
 )
-
+# 采购单明细表
 create table purchase_detail(
 	purchase_id varchar(8) not null,
 	product_id varchar(8) not null,
@@ -50,7 +52,7 @@ create table purchase_detail(
 	purchase_amount decimal(18,2) not null,
 	constraint pk_purchase_product primary key(purchase_id, product_id)
 )
-
+# 销售单主表
 create table delivery_master(
 	delivery_id varchar(8) primary key,
 	delivery_date datetime not null,
@@ -58,7 +60,7 @@ create table delivery_master(
 	delivery_address varchar(100) not null,
 	subtotal decimal(18,2) not null
 )
-
+# 销售单明细表
 create table delivery_detail(
 	delivery_id varchar(8) not null,
 	product_id varchar(8) not null,
@@ -67,3 +69,27 @@ create table delivery_detail(
 	sales_amount decimal(18,2) not null,
 	constraint pk_deliver_product primary key(delivery_id, product_id)
 )
+
+# 系统功能部分的表
+# 用户表
+create table drp_sys_user (
+  id int(32) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  company_id int(32) NOT NULL COMMENT '归属公司',
+  office_id int(32) NOT NULL COMMENT '归属部门',
+  login_name varchar(100) NOT NULL COMMENT '登录名',
+  password varchar(100) NOT NULL COMMENT '密码',
+  no varchar(100) DEFAULT NULL COMMENT '工号',
+  name varchar(100) NOT NULL COMMENT '姓名',
+  email varchar(200) DEFAULT NULL COMMENT '邮箱',
+  phone varchar(200) DEFAULT NULL COMMENT '电话',
+  mobile varchar(200) DEFAULT NULL COMMENT '手机',
+  create_by varchar(64) NOT NULL COMMENT '创建者',
+  create_date datetime NOT NULL COMMENT '创建时间',
+  update_by varchar(64) NOT NULL COMMENT '更新者',
+  update_date datetime NOT NULL COMMENT '更新时间',
+  remarks varchar(255) DEFAULT NULL COMMENT '备注信息',
+  PRIMARY KEY (id)
+)
+
+
+
--- drp/readme.txt
+++ drp/readme.txt
@@ -1,11 +1,67 @@
 drp分销系统:
+功能模块：
+1)系统功能
+	* 用户管理
+	* 角色管理
+	* 菜单管理
+	* 区域管理
+	* 部门管理（机构管理和区域管理有时候可以放在一起）
+	* 字典管理
+	* 日志管理
+2)业务功能
+	* 客户管理
+	* 商品管理
+	* 供应商管理
+	* 采购单管理
+	* 采购单明细管理
+	* 销售单管理
+	* 销售单明细管理
 
-1. 将所有的页面放到WEB-INF文件夹下保存起来
+项目表结构：
+1)系统功能部分
+	* 用户表
+	* 角色表
+	* 部门表
+	* 区域表
+	* 菜单表
+	* 字典表
+	* 日志表
+2)业务功能部分
+	* 客户表
+	* 商品表
+	* 供应商表
+	* 采购单主表
+	* 采购单明细表
+	* 销售单主表
+	* 销售单明细表
 
-2. 登录成功后一定要重定向到主页面，不能转发，转发的话以刷新就会重新登录
+项目的后台架构：spring+struts2+mybatis
 
+项目的前台页面：待完善...
 
-
-
+项目的后台包结构：
+	com.qjl.drp.util
+	com.qjl.drp.common
+	com.qjl.drp.test
+	com.qjl.drp.sysmanage.entity
+	com.qjl.drp.sysmanage.mapper
+	com.qjl.drp.sysmanage.service
+	com.qjl.drp.sysmanage.action
+项目的前台页面结构:
+	webapp/index.jsp 引导页面，通过后台跳转到登陆页面
+	webapp/jsAndCss/js 一些js文件
+	webapp/jsAndCss/css 一些css文件
+	webapp/jsAndCss/img 一些图片文件
+	webapp/WEB-INF/pages 放页面
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 
