dcf87a5f8dc8db539956c34972d3b3cd38d44122
MrQuJL <909301740@qq.com>
2018-02-09 17:27:20 +0800

    记录日志思路：通过aspectJ向有关方法（add, update, del）织入切面获取用户操作，插入数据库

---
+++ drp/src/main/java/com/lyu/drp/util/LogAspectJUtils.java
@@ -0,0 +1,62 @@
+package com.lyu.drp.util;
+
+import java.sql.Timestamp;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAspectJUtils {
+	
+	/**
+	 * 记录添加方法的切面
+	 * @param 
+	 * @return
+	 */
+	@AfterReturning(value="execution(* com.lyu.drp.sysmanage.service.impl.*.add*(..))", returning="result")
+	public void addAfterAdvice(JoinPoint joinPoint, Object result) {
+		String userName = UserUtils.getCurrentUser().getUserName();
+		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+		int operateType = 1;
+		String operatorMethod = joinPoint.getSignature().getName();
+		
+		System.out.println("用户-" + userName + "-在" + currentTime + "通过" + operatorMethod + "-增加xx");
+		
+	}
+	
+	/**
+	 * 记录删除方法的切面
+	 * @param 
+	 * @return
+	 */
+	@AfterReturning(value="execution(* com.lyu.drp.sysmanage.service.impl.*.del*(..))", returning="result")
+	public void delAfterAdvice(JoinPoint joinPoint, Object result) {
+		String userName = UserUtils.getCurrentUser().getUserName();
+		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+		int operateType = 2;
+		String operatorMethod = joinPoint.getSignature().getName();
+		
+		System.out.println("用户-" + userName + "-在" + currentTime + "通过" + operatorMethod + "-删除xx");
+		
+	}
+	
+	/**
+	 * 记录修改方法的切面
+	 * @param 
+	 * @return
+	 */
+	@AfterReturning(value="execution(* com.lyu.drp.sysmanage.service.impl.*.update*(..))", returning="result")
+	public void updateAfterAdvice(JoinPoint joinPoint, Object result) {
+		String userName = UserUtils.getCurrentUser().getUserName();
+		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+		int operateType = 3;
+		String operatorMethod = joinPoint.getSignature().getName();
+		
+		System.out.println("用户-" + userName + "-在" + currentTime + "通过" + operatorMethod + "-修改xx");
+		
+	}
+	
+}
