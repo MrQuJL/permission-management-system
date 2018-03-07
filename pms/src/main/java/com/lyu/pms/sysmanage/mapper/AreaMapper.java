85e8174a0a0c79df6a8f879f7ebe06f5231e4332
MrQuJL <909301740@qq.com>
2018-03-07 09:30:50 +0800

    为 AreaMapper 和 DeptMapper 的接口完善注释（之前没写的参数和返回值部分）

--- pms/src/main/java/com/lyu/pms/sysmanage/mapper/AreaMapper.java
+++ pms/src/main/java/com/lyu/pms/sysmanage/mapper/AreaMapper.java
@@ -17,58 +17,56 @@ public interface AreaMapper {
 	
 	/**
 	 * 获取区域的详细信息,包括父级区域的名称
-	 * @param 
-	 * @return
+	 * @param id 区域的id
+	 * @return 对应的区域信息
 	 */
 	AreaDto getAreaDetailById(Long id);
 	
 	/**
 	 * 查询全部的区域列表
-	 * @param 
-	 * @return
+	 * @return 全部的区域列表
 	 */
 	List<Area> getAllAreaList();
 	
 	/**
 	 * 根据角色id获取区域列表
-	 * @param 
-	 * @return
+	 * @param roleId 角色id
+	 * @return 该角色id所拥有的区域列表
 	 */
 	List<Area> getAreaListByRoleId(Long roleId);
 	
 	/**
 	 * 通过父级区域id获取所有的子区域
-	 * @param 
-	 * @return
+	 * @param pId 父级区域id
+	 * @return 该父级区域的所有子区域
 	 */
 	List<Area> getSubAreaByPId(Long pId);
 	
 	/**
 	 * 统计当前区域下的直接子区域的数量
-	 * @param 
-	 * @return
+	 * @param areaId 区域id
+	 * @return 当前区域下的直接子区域的数量
 	 */
 	int countSubArea(Long areaId);
 	
-	
 	/**
 	 * 添加区域
-	 * @param 
-	 * @return
+	 * @param area 要添加到数据库中的记录
+	 * @return 受影响的行数，返回值为1则添加成功，否则失败
 	 */
 	int saveArea(Area area);
 	
 	/**
 	 * 修改区域
-	 * @param 
-	 * @return
+	 * @param area 要修改的区域
+	 * @return 受影响的行数，返回值为1则修改成功，否则失败
 	 */
 	int updateArea(Area area);
 	
 	/**
 	 * 删除子区域（逻辑删除）
-	 * @param 
-	 * @return
+	 * @param areaId 要被删除的区域id
+	 * @return 受影响的行数，返回值为1则修改成功，否则失败
 	 */
 	int delArea(Long areaId);
 	
--- pms/src/main/java/com/lyu/pms/sysmanage/mapper/DeptMapper.java
+++ pms/src/main/java/com/lyu/pms/sysmanage/mapper/DeptMapper.java
@@ -16,58 +16,57 @@ import com.lyu.pms.sysmanage.entity.Dept;
 public interface DeptMapper {
 	
 	/**
-	 * 获取部门的详细信息，包括父部门的名称...
-	 * @param 
-	 * @return
+	 * 获取部门的详细信息，包括父部门的名称
+	 * @param deptId
+	 * @return 部门的详细信息
 	 */
 	DeptDto getDeptDetailById(Long deptId);
 	
 	/**
 	 * 获取所有的部门列表
-	 * @param 
-	 * @return
+	 * @return 所有的部门列表
 	 */
 	List<Dept> getAllDeptList();
 	
 	/**
-	 * 根据角色id获取该角色所拥有的部门权限
-	 * @param 
-	 * @return
+	 * 根据角色id获取该角色所拥有的部门
+	 * @param roleId 角色id
+	 * @return 该角色所能够操作的部门
 	 */
 	List<Dept> getDeptListByRoleId(Long roleId);
 	
 	/**
 	 * 获取当前部门的所有子部门id
-	 * @param 
-	 * @return
+	 * @param parentId 父部门的id
+	 * @return parentId下的所有子部门的id
 	 */
 	List<Long> getAllSubDeptIds(Long parentId);
 	
 	/**
 	 * 统计父部门下面的子部门的数量
-	 * @param 
-	 * @return
+	 * @param deptId 当前的部门id
+	 * @return deptId部门下的所有子部门的数量
 	 */
 	int countSubDeptByPId(Long deptId);
 	
 	/**
 	 * 添加部门
-	 * @param 
-	 * @return
+	 * @param dept 要添加的部门
+	 * @return 受影响的记录数，1表示添加成功，否则失败
 	 */
 	int saveDept(Dept dept);
 	
 	/**
 	 * 修改部门
-	 * @param 
-	 * @return
+	 * @param dept 要修改的部门
+	 * @return 受影响的记录数，1表示修改成功，否则失败
 	 */
 	int updateDept(Dept dept);
 	
 	/**
-	 * 删除部门（逻辑删除）
-	 * @param 
-	 * @return
+	 *  删除部门（逻辑删除）
+	 * @param deptId 要逻辑删除的部门
+	 * @return 受影响的记录数，1表示逻辑删除成功，否则失败
 	 */
 	int delDept(Long deptId);
 	
